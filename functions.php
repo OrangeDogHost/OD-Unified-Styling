1c8f6e49fc39f3e6e0bee0681ee86065b7c1fdc9
Zack Palmer <zacnoo@gmail.com>
2020-05-12 13:46:09 +1200

    Added file to add custom site-specfic CSS - 0.0.6 release

--- functions.php
+++ functions.php
@@ -2,4 +2,7 @@
 function my_theme_enqueue_styles() { 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_custom_styles() { 
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/custom.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 'my_theme_enqueue_custom_styles' );
